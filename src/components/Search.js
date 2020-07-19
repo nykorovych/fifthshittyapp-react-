dc9794b7f0521a4b0948438d12dab9ad94afb7ff
Iurii <ny.iuriii@gmail.com>
2020-07-19 23:53:33 +0200

    sd

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,43 +1,55 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("cat");
-  const [results, setResult] = useState([])
-
+  const [results, setResult] = useState([]);
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
 
   useEffect(() => {
-    const search = async () => {
-       const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
-            params: {
-                action: 'query',
-                list: 'search',
-                origin: '*',
-                format: 'json',
-                srsearch: term
-
-            }
-        })
-        setResult(data.query.search)
-    }
-    search()
+    const timerID = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timerID);
+    };
+  }, [term]);
   
+  useEffect(() => {
+    const search = async () => {
+        const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+          params: {
+            action: "query",
+            list: "search",
+            origin: "*",
+            format: "json",
+            srsearch: debouncedTerm,
+          },
+        });
+        setResult(data.query.search);
+      };
+      search()
+  }, [debouncedTerm])
 
-  }, [term])
-  const renderedResults = results.map(i => {
-      return (
-          <div className="item" key={i.pageid}>
-              <div className="content">
-                <div className="header">
-                    {i.title}
-                </div>
-                <span dangerouslySetInnerHTML={{__html: i.snippet}}>
-
-                </span>
-              </div>
-          </div>
-        )
-  })
+  
+  const renderedResults = results.map((i) => {
+    return (
+      <div className="item" key={i.pageid}>
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${i.pageid}`}
+            className="ui button"
+          >
+            GO
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{i.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: i.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
       <div className="ui form">
@@ -50,9 +62,7 @@ const Search = () => {
           ></input>
         </div>
       </div>
-      <div className="ui celled list">
-        {renderedResults}
-      </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
